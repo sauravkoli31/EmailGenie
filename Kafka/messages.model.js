9783c2f0522e44e5aca9ebe0e406279be3ad7286
sauravkoli-alef <60574727+sauravkoli-alef@users.noreply.github.com>
2021-04-15 14:46:44 +0400

    Adding Kafka scripts

---
+++ Kafka/messages.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const messagesSchema = new Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  email_uuid: {
+    type: String,
+    required: false
+  },
+  fromAddr: {
+    type: String,
+    require:false
+  },
+  rootDomain: {
+    type: String,
+    require:false
+  },
+  unsubLink: [{
+    type: String,
+    require:false
+  }]
+}, {
+  timestamps: { createdAt: 'created_at' },
+});
+
+const Messages = mongoose.model('messages', messagesSchema);
+
+module.exports = Messages;
